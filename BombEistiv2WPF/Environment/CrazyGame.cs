4aff349f7401a05cec0efa572ba3e648d63eea4b
BenouKat <ben.goze@gmail.com>
2012-01-23 10:29:24 +0100

    BombEisti v1.2
    Release officielle
    
    Signed-off-by: BenouKat <ben.goze@gmail.com>

--- BombEistiv2WPF/Environment/CrazyGame.cs
+++ BombEistiv2WPF/Environment/CrazyGame.cs
@@ -1,6 +1,13 @@
-﻿namespace BombEistiv2WPF.Environment
+﻿using System;
+using System.Timers;
+using System.Windows.Threading;
+using BombEistiv2WPF.View;
+using BombEistiv2WPF.View.Menu;
+
+namespace BombEistiv2WPF.Environment
 {
     public class CrazyGame : Game
     {
+        
     }
 }
